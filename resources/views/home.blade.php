fb1ce654aa38acfe168cd767328bd113319c4c2f
gifari-dev <rahmailham79@gmail.com>
2025-11-07 14:29:27 +0700

    Tambah tampilan frontend: halaman home dan navbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Project UTS Saya</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+<body>
+
+    <div class="container mt-5">
+        <h1 class="text-primary">Halo, ini hasil edit project gifari!</h1>
+        <p>Halaman ini sekarang sudah menggunakan bootstrap.</p>
+        <button class="btn btn-success">ini tombol gifari</button>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
